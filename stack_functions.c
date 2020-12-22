5f80530d5d69cc414740478c9e30143a2b4d843d
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2020-12-22 15:37:03 +0000

    Add comments to pall function

--- stack_functions.c
+++ stack_functions.c
@@ -58,6 +58,13 @@ void push(stack_t **stack, unsigned int line_number/* , char *n */)
 	*stack = new;
 }
 
+/**
+ * pall - Prints all the values on the stack,
+ * starting from the top of the stack.
+ * @stack: Double pointer to the head of the Stack.
+ * @line_number: Number of the line.
+ * Return: Nothing.
+ */
 void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *node = *stack;
