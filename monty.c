e7a45a0dfb75c6592e71c289dca62b3f1d2daa4e
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2020-12-22 14:17:54 +0000

    Main function Update It receives the arguments from the test files in the bytecodes directory. Open the monty file in read mode and check line by line,separating arguments with the strtok function and then call the opcode_struct function to check and execute each argument found.

--- monty.c
+++ monty.c
@@ -8,5 +8,41 @@
  */
 int main(int argc, char *argv[])
 {
+	FILE *fd = NULL;
+	stack_t *stack = NULL;
+	char *argument = NULL, *line = NULL;
+	size_t lineSize = 0;
+	unsigned int line_number = 0; 
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		return (EXIT_FAILURE);
+	}
+	fd = fopen(argv[1], "r");
+	if (fd == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (getline(&line, &lineSize, fd) != EOF)
+	{
+		line_number++;
+		argument = strtok(line, DELIMITERS);
+		if (argument == NULL)
+		{
+			free(argument);
+			continue;
+		}
+		else if (*argument == '#')
+		{
+			continue;
+		}
+		opcode_struct(argument, &stack, line_number);
+	}
+	if (line)
+		free(line);
+	fclose(fd);
 	
+	return (0);
 }
