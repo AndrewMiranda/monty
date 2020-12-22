8384e095e864f7ea6b55fd3e159da02e4316a4d6
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2020-12-22 15:35:04 +0000

    Add free_stack function

---
+++ util_functions.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * free_stack - Free a stack.
+ * @stack: Head node.
+ * Return: Nothing.
+ */
+void free_stack(stack_t *stack)
+{
+	stack_t *current = stack;
+
+	if (current != NULL)
+	{
+		free_stack(current->next);
+		free(current);
+	}
+}
+
+/**
+ * void free_all(stack_t *stack, char *line, FILE *fp)
+ * {
+ *
+ * }
+*/
