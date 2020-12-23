57ffa4099fe15213cbad63f384a387dbd6718d3e
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2020-12-23 02:05:05 +0000

    Created: _add function

---
+++ calc_functions.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * add - This function adds the top two elements of the stack.
+ * @stack: Double pointer to the head of the Stack.
+ * @line_number: Number of the line.
+ * Return: Nothing.
+ */
+void _add(stack_t **stack, unsigned int line_number)
+{
+        stack_t *node = NULL;
+        int sum = 0;
+
+        if (!stack || !(*stack) || !(*stack)->next)
+        {
+                fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+
+        node = *stack;
+        sum += node->n + node->next->n;
+        node->next->n = sum;
+        pop(stack, line_number);
+}
